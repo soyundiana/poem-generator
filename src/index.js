6cf49f968476ca8358e29076deb056a92c6e3114
soyundiana <142823980+soyundiana@users.noreply.github.com>
2024-04-19 13:50:36 +0100

    added api integration! (+axios, poem styling)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+function displayPoem (response) {
+   new Typewriter('#poem', {
+  strings: response.data.answer,
+  autoStart: true,
+  delay: 5,
+  cursor: ""
+});
+}
+
+
 function generatePoem(event) {
     event.preventDefault();
 
-   new Typewriter('#poem', {
-  strings: "Tere Tulemast",
-  autoStart: true,
-  delay: 1,
-  cursor: ""
-});
+    let requestInput = document.querySelector("#user-request");
+    let apiKey = "3t6dd836d3b2360o01bbc44d5dfa374c";
+    let prompt = `Generate an estonian poem about ${requestInput.value}`;
+    let context = "You are a very intellegent AI that likes puns in Estonian poems. Please generate a 4 line poem in basic HTML and separate each line with a <br />. Make each line height 2. Do not exceed line height limit. Do not include title to the poem.";
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiURL).then(displayPoem);
+
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
